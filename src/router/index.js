4df7d47ec68acd57433e6d575b13942d8d8e6162
chulin <wxhllt@163.com>
2020-12-18 10:49:58 +0800

    12.17上传的版本

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
+import films_router from '@/router/routes/films'
+import cinema_router from '@/router/routes/cinema'
+import mine_router from '@/router/routes/mine'
+
 const routes = [
+  films_router,
+  cinema_router,
+  mine_router,
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    redirect: '/films'
   }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
---
+++ src/router/routes/mine.js
@@ -0,0 +1,8 @@
+import mine from '@/views/mine/mine';
+
+var mine_router = {
+    path: '/mine',
+    component: mine
+};
+
+export default mine_router
